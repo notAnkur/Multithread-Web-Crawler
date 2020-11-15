c92f57e53db8c36e5f9e6fdb8dcbb3770952273e
Ankur Anant <ankuranant99@gmail.com>
2020-11-16 01:24:21 +0530

    initial ui

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Multithread-Web-Crawler
+## OS Project (CSE 301)
+
+#### Multi-threaded Web Crawler: Implement a multi-threaded web crawler. The crawler should be able to remember the last URLs and able to resume. Your program should be able to create appropriate number of threads.
---
+++ src/webcrawlie/WebCrawlie.java
@@ -0,0 +1,16 @@
+package webcrawlie;
+
+/**
+ *
+ * @author Ankur Anant <hello@ankuranant.dev>
+ */
+public class WebCrawlie {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
